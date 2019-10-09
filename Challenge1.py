ef5a0921440095ab0cddd71b31d1e0f54f8aaeac
verlebie <you@example.com>
2019-10-09 19:05:29 +0800

    test

---
+++ Challenge1.py
@@ -0,0 +1,3 @@
+#Create KM by QnA Maker.
+# Use the Microsoft QnA Maker cognitive service to build and publish a knowledge base that encapsulates the information in the FAQ document.
+# Write test code that consumes your knowledge base.
